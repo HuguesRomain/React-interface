4d48e02ecf07a4fada86986edbf7b33d2b765e63
HuguesRomain <huguesromainpro@gmail.com>
2019-02-02 18:50:25 +0100

    AppArray

--- src/App.js
+++ src/App.js
@@ -3,35 +3,20 @@ import Element from './components/Element';
 import Bottom from './components/Bottom';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <Fragment>
       <div className='interface'>
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
-      <Element />
+        <Element />
+        <Element />
+        <Element />
+        <Element />
+        <Element />
+        <Element />
+        <Element />
+        <Element />
       </div>
       <Bottom />
       </Fragment>
--- src/components/NameApp.js
+++ src/components/NameApp.js
@@ -1,9 +1,97 @@
 import React, { Fragment, Component } from 'react'
 
+const apps = {
+  app1: {
+    name: 'Messenger',
+  },
+  app2: {
+    name: 'YouTube',
+  },
+  app3: {
+    name: 'Pinterest',
+  },
+  app4: {
+    name: 'Discord',
+  },
+  app5: {
+    name: 'Steam',
+  },
+  app6: {
+    name: 'Snapchat',
+  },
+  app6: {
+    name: 'Twitter',
+  },
+  app7: {
+    name: 'Instagram',
+  },
+  app8: {
+    name: 'Amazon',
+  },
+  app9: {
+    name: 'WhatsApp',
+  },
+  app10: {
+    name: 'Reddit',
+  },
+  app11: {
+    name: 'LinkedIn',
+  },
+  app12: {
+    name: 'Netflix',
+  },
+  app13: {
+    name: 'Facebook',
+  },
+  app14: {
+    name: 'Tinder',
+  },
+  app15: {
+    name: 'SoundCloud',
+  },
+  app16: {
+    name: 'Dribbble',
+  },
+  app17: {
+    name: 'Uber',
+  },
+  app18: {
+    name: 'PayPal',
+  },
+  app19: {
+    name: 'Calm',
+  },
+  app20: {
+    name: 'test',
+  },
+  app21: {
+    name: 'test',
+  },
+  app22: {
+    name: 'test',
+  },
+  app23: {
+    name: 'test',
+  },
+}
+
 class NameApp extends Component{
+  state = {
+    apps,
+    }
   render(){
+   const { apps } = this.state;
+
+
+    const AppInterface = Object.keys(apps)
+    .map(app  => (
+       <div
+       key={app}
+       name= {apps[app].name} />
+    ))
+
     return(
-    <div className='nameApp'>name</div>
+    <div className='nameApp'> name </div>
     )
   }
 }
